9fa684d23d4468611f7b34f67a71b72ce6222928
Alexandre M <Alexandre.molesti@hotmail.com>
2026-05-29 11:51:07 +0200

    app reactjs archivé sur le drive

--- Application/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'abstergo.chase',
-  appName: 'abstergo-chase',
-  webDir: 'dist'
-};
-
-export default config;
